6880fc6c24080d0bb28c68a03ab421279d74a89e
Igor Shevchenko <cyberskunk@gmail.com>
2015-11-11 00:07:54 +0300

    convert Bullet to ES2015 class

--- src/Bullet.js
+++ src/Bullet.js
@@ -1,16 +1,15 @@
 'use strict';
-var Bullet = function(center, velocity, color) {
-    this.size = { x: 3, y: 3 };
-    this.center = center;
-    this.velocity = velocity;
-    this.color = color;
-};
 
-Bullet.prototype = {
+export default class Bullet {
+        constructor (center, velocity, color) {
+        this.size = { x: 3, y: 3 };
+        this.center = center;
+        this.velocity = velocity;
+        this.color = color;
+    }
+
     update () {
         this.center.x += this.velocity.x;
         this.center.y += this.velocity.y;
     }
 };
-
-export default Bullet;
